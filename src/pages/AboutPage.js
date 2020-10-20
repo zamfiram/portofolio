6326fd72838a3ca9c386ab7c61bb21bb157b3c04
Madalina <madalina.zamfira@gmail.com>
2020-10-20 22:34:07 +0200

    changed the footer display

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -6,13 +6,13 @@ import resume from '../assets/images/Resume.jpg';
 function AboutPage(props) {
 
     return(
-        <div>
+        <div className="about-page">
             <Hero title={props.title} />
 
             <Content>
             <p>Hi,</p>
             <p>I'm Madalina Zamfira, a 29 years old javascript enthusiast. In 2020 I've been studying coding at Wild Code School. Here I've been working on projects using HTML, CSS, JavaScript, React, nodeJS, Git, Bootstrap and MySql. The projects have been managed using the SCRUM methodology.</p>
-            <p>You can read more about my biography, experience, skills, education and much more in the PDF here below</p>
+            <p>You can read more about my biography, experience, skills, education and much more in the PDF here below:</p>
             <a href={resume} target="_blank">My resume</a>
             </Content>
         </div>
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -81,7 +81,7 @@ class ContactPage extends React.Component {
                            <Form.Control id="message" name="message" as="textarea"rows="3" value={this.state.message} onChange={this.handleChange}/>
                        </Form.Group>
 
-                       <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled} >
+                       <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled} style={{ backgroundColor: '#2B2D42'}} >
                            Send
                        </Button>
 
